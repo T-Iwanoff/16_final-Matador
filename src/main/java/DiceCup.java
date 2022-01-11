523c7211fdec4f0e1f085eab6aabb1015a2127ce
Thor Jensen <thor@iwanoff.dk>
2022-01-11 12:58:39 +0100

    Added classes Die and DiceCup

--- src/main/java/DiceCup.java
+++ src/main/java/DiceCup.java
@@ -1,2 +1,38 @@
+
 public class DiceCup {
+    /** The face value of the first die */
+    private int faceVal1;
+    /** The face value of the second die */
+    private int faceVal2;
+    /** The two die objects used in the class */
+    private Die die1, die2;
+
+    /** Constructor. Initiates two die objects */
+    public DiceCup() {
+        Die die1 = new Die(6);
+        Die die2 = new Die(6);
+    }
+
+    /** Rolls two dice and returns the sum */
+    public int rollDice() {
+        faceVal1 = die1.rollDie();
+        faceVal2 = die2.rollDie();
+        return faceVal1+faceVal2;
+    }
+
+    /** Returns the face value of both die */
+    public int[] getFaces() {
+        int[] faces = {faceVal1,faceVal2};
+        return faces;
+    }
+
+    /** Returns the sum of both die */
+    public int getSum() {
+        return faceVal1+faceVal2;
+    }
+
+    /** Returns whether the two die have the same face value */
+    public boolean isPair() {
+        return (faceVal1==faceVal2);
+    }
 }
--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,2 +1,33 @@
+
 public class Die {
+    /** Number of faces on the die */
+    private int faces;
+    /** The face the dice is currently showing */
+    private int showingFace = 1;
+
+    /**
+     * Initializes a new die.
+     *
+     * @param faces Number of faces on the die.
+     */
+    public Die(int faces)
+    {
+        this.faces = faces;
+    }
+
+    /**
+     * Rolls the die.
+     *
+     * @return Face value of the die.
+     */
+    public int rollDie()
+    {
+        showingFace = (int) (Math.random() * faces) + 1;
+        return showingFace;
+    }
+
+    public int getShowingFace()
+    {
+        return showingFace;
+    }
 }
