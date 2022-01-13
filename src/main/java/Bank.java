4971ca8ecc988572ca2776cfb2a59a3b5f97ab84
Jackie <Jackie_Trinh@hotmail.com>
2022-01-13 17:16:19 +0100

    Bank - Pay to field made

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,35 @@
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
+
+/** The PlayerManager keep track of all players.  */
+
 public class Bank {
+
+    /** Pays for the field. */
+    public void buyField() {
+        int fieldPrice = FieldManager.getPrice();
+        int playerBalance = PlayerManager.getBalance();
+
+        int total = playerBalance - fieldPrice;
+
+        PlayerManager.setBalance(total);
+    }
+
+
+    /** Pay rent of the field to the owner. */
+
+    /** Change player balance. */
+
+
+    /** return the sum of all players non-liquid assets. */
+
+
+
+    /**
+     * Changes the balance of the player
+     * player the player object whose balance will be changed
+     * change the change in balance
+     */
+
 }
