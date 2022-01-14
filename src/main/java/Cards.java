0117f9c791d38c9939d20e974cd08190d5b3a611
Jackie <Jackie_Trinh@hotmail.com>
2022-01-14 22:58:29 +0100

    cards and deck classes made

---
+++ src/main/java/Cards.java
@@ -0,0 +1,10 @@
+public class Cards {
+    private int identification, action, value;
+
+    /** Constructor for cards */
+    public Cards(int ID,int act, int val) {
+        this.identification = ID;
+        this.action = act;
+        this.value = val;
+    }
+}
