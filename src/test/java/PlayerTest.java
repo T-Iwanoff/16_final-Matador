ef48476bdeed858a8fa74ca2dc6999039e064415
Thor Jensen <thor@iwanoff.dk>
2022-01-16 13:11:45 +0100

    Added test for Player Fixed junit dependency

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,36 @@
+import org.junit.*;
+import org.junit.Assert;
+
+public class PlayerTest {
+
+    private Player player;
+
+    @Before
+    void setUp() throws Exception {
+        player = new Player("tester");
+    }
+
+    @Test
+    void testConstructor() {
+        Assert.assertEquals("tester",player.getName()); //Player should get the name specified in the setUp
+        Assert.assertEquals(0,player.getPosition()); //Player should that at position 0
+    }
+
+
+    @Test
+    void testMovePlayer() {
+        player.movePlayer(5);
+        Assert.assertEquals(5,player.getPosition()); //The player should have moved 5 fields
+        player.movePlayer(6);
+        Assert.assertEquals(11,player.getPosition()); //The position should be additive (5+6)
+        player.setPosition(25);
+        player.movePlayer(7);
+        Assert.assertEquals(32,player.getPosition()); //The method should accept outside changes in the player's position
+        player.movePlayer(10);
+        Assert.assertEquals(2,player.getPosition()); //Reaching 40 should reset the position to 0
+
+    }
+
+
+
+}
